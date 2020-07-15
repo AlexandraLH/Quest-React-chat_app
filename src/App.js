b73da4a5a4e6940bb34dcb0837bea9ad141152ca
AlexandraLH <alexandra.lhermitte@icloud.com>
2020-07-15 16:21:14 +0200

    quest2_2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import Contact from './components/Contact.js';
 
 function App() {
   return (
-    <Contact/>
+    <div>
+    <Contact 
+      name="Bree Van de Kamp"
+      avatar="https://randomuser.me/api/portraits/women/2.jpg"
+      status={true} 
+    />
+
+    <Contact 
+      name="Gabriel Solis"
+      avatar="https://randomuser.me/api/portraits/women/14.jpg"
+      status={false}
+    />
+
+    <Contact 
+      name="Lynette Scavo"
+      avatar="https://randomuser.me/api/portraits/women/96.jpg"
+      status={true}
+    />
+  </div>
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import './Contact.css';
-function Contact() {
+import PropTypes from 'prop-types';
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  status: PropTypes.bool.isRequired,
+};
+
+function Contact(props) {
     return (
       <div className="Contact">
-          <img className="avatar" src="https://randomuser.me/api/portraits/women/75.jpg"/>
+        <img className="avatar" src={props.avatar} alt="avatar"/>
           <div>
-            
+                <h2 className="name">
+                  {props.name} </h2>
+                    
+               
                 <div className="status">
-                    <div className="status-online"></div>
+                    <div className={props.status ? "status-online" : "status-offline"}></div>
                     <p className="status-text">
-                    Online
-                    </p>
+                    {props.status ? 'online' : 'offline'}</p>
+                        
+                    
                 </div>
           </div>
       </div>
     );
+          
+ 
   }
 
   export default Contact;
